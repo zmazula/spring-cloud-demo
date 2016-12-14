b388bf29efaa5950497a820fd38c9238a9db7e54
Zoltan Mazula <mazulazoltan@gmail.com>
2016-12-14 10:43:44 +0100

    ldap extension

--- zuul-proxy/src/main/java/com/epam/demo/security/WebSecurityConfig.java
+++ zuul-proxy/src/main/java/com/epam/demo/security/WebSecurityConfig.java
@@ -1,6 +1,8 @@
 package com.epam.demo.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,8 +12,33 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 /**
  * Created by zmazula on 08/11/16.
  */
+@Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${authentication-provider}")
+    private String authenticationProvider;
+
+    @Value("${ldap-user-search-field}")
+    private String ldapUserSearchField;
+
+    @Value("${ldap-user-search-base}")
+    private String ldapUserSearchBase;
+
+    @Value("${ldap-group-search-base}")
+    private String ldapGroupSearchBase;
+
+    @Value("${ldap-url}")
+    private String ldapUrl;
+
+    @Value("${ldap-port}")
+    private int ldapPort;
+
+    @Value("${ldap-manager-dn}")
+    private String ldapManagerDn;
+
+    @Value("${ldap-manager-password}")
+    private String ldapManagerPassword;
 
     @Autowired
     MidAuthenticationProvider midAuthenticationProvider;
@@ -20,8 +47,24 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     CookieCsrfTokenRepository cookieCsrfTokenRepository;
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(midAuthenticationProvider);
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        switch (authenticationProvider) {
+            case "ldap":
+                auth
+                    .ldapAuthentication()
+                    .userSearchFilter("("+ldapUserSearchField+"={0})")
+                    .userSearchBase(ldapUserSearchBase)
+                    .groupSearchBase(ldapGroupSearchBase)
+                    .contextSource()
+                    .url(ldapUrl)
+                    .port(ldapPort)
+                    .managerDn(ldapManagerDn)
+                    .managerPassword(ldapManagerPassword);
+                break;
+            default:
+                auth.authenticationProvider(midAuthenticationProvider);
+                break;
+        }
     }
 
     @Override
@@ -35,5 +78,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin().loginPage("/login.jsp").permitAll();
     }
-
 }
+
